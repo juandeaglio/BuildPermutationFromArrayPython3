9bb9fbb9b67e0a1983968e9a29e3ad97bfdf40c8
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-10-12 14:57:01 -0400

    Think of a test case

---
+++ Test_BuildArrayFromPermutation.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class BuildArrayFromPermutation(unittest.TestCase):
+    def test_think_of_first_test_case(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def build_array(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    build_array('PyCharm')
